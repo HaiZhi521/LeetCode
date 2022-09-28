1dc0991131d8e8ab73f89da7e9167c0ee04f16d7
llk <507337979@qq.com>
2022-09-28 15:26:51 +0800

    1

--- src/Study_5_10/Data.java
+++ src/Study_5_10/Data.java
@@ -1,7 +1,34 @@
 package src.Study_5_10;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Data {
     public static void main(String[] args) {
-        System.out.println(new Data());
+//        String year;
+//        String week;
+//        String month;
+//        String day;
+//        String hour;
+//        String minutes;
+//        String seconds;
+//        SimpleDateFormat time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        Date oldTime = new Date();
+//        String expireTime = time.format(new Date(oldTime.getTime() + 30*1000));
+//        year = expireTime.indexOf(0,1);
+//        String cron =
+
+        BigDecimal bigDecimal = new BigDecimal(100);
+        System.out.println(bigDecimal);
+        String sdf = "17 38 14 25 07 ? 2022";
+        String s = sdf.substring(6,8)+":"+sdf.substring(3,5);
+        //SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+//        Date date = new Date();
+//        String formatTimeStr = null;
+//        if (date != null) {
+//            formatTimeStr = sdf.format(date.getTime() + 30 * 1000);
+//        }
+        System.out.println(s);
     }
 }
--- src/Study_5_10/LocalData.java
+++ src/Study_5_10/LocalData.java
@@ -1,16 +1,22 @@
 package src.Study_5_10;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class LocalData {
     public static void main(String[] args) {
-        LocalDate today = LocalDate.of(1999,12,31);
-        int year = today.getYear();
-        int month = today.getMonthValue();
-        int day = today.getDayOfMonth();
-        System.out.println(year);
-        System.out.println(month);
-        System.out.println(day);
-        System.out.println(today);
+        String s = "hello";
+        System.out.println(s.matches(".*he.*"));
+        Date date = new Date();
+        date.toString();
+        System.out.println(date);
+//        LocalDate today = LocalDate.of(1999,12,31);
+//        int year = today.getYear();
+//        int month = today.getMonthValue();
+//        int day = today.getDayOfMonth();
+//        System.out.println(year);
+//        System.out.println(month);
+//        System.out.println(day);
+//        System.out.println(today);
     }
 }
